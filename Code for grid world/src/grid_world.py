2b1a3f5a4f2916e281accf1578c163c5097dabbe
Yize Mi <miyize@westlake.edu.cn>
2024-03-12 15:52:50 +0800

    docs: delete comment

--- Code for grid world/src/grid_world.py
+++ Code for grid world/src/grid_world.py
@@ -6,7 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches          
 from examples.arguments import args           
-# from src.utils import *
 
 class GridWorld():
 
